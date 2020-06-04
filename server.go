581fcf24bf5d2aa5c63d6965072520f0a6dde780
Davy Van Den Steen <davy.vandensteen@showpad.com>
2020-06-04 19:00:04 +0200

    Fmt

--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/di-wu/scim-filter-parser"
+	filter "github.com/di-wu/scim-filter-parser"
 	"github.com/elimity-com/scim/errors"
 	"github.com/elimity-com/scim/schema"
 )
