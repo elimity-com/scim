da6d83372ab2dc0a613bdc547836db89621b3cb9
Davy Van Den Steen <davy.vandensteen@showpad.com>
2020-06-05 10:58:11 +0200

    Added docs

--- schema/common.go
+++ schema/common.go
@@ -1,5 +1,6 @@
 package schema
 
+// Keys for the Common Attributes: https://tools.ietf.org/html/rfc7643#section-3.1
 const (
 	CommonAttributeID         = "id"
 	CommonAttributeExternalID = "externalId"
