7ae555a8a7200220d69199a486d378ce5a5b3fb5
kskumgk63 <keisuke.umegaki.630@gmail.com>
2020-11-24 19:03:56 +0900

    update the comment

--- resource_handler.go
+++ resource_handler.go
@@ -101,6 +101,10 @@ type ResourceHandler interface {
 	Delete(r *http.Request, id string) error
 	// Patch update one or more attributes of a SCIM resource using a sequence of
 	// operations to "add", "remove", or "replace" values.
-	// If you return empty Resource.Attributes, 204 No Content will be response.
+	// If you return no Resource.Attributes, a 204 No Content status code will be returned.
+	// This case is only valid in the following scenarios:
+	// 1. the Add/Replace operation should return No Content only when the value already exists AND is the same.
+	// 2. the Remove operation should return No Content when the value to be remove is already absent.
+	// More information in Section 3.5.2 of RFC 7644: https://tools.ietf.org/html/rfc7644#section-3.5.2
 	Patch(r *http.Request, id string, request PatchRequest) (Resource, error)
 }
