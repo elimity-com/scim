4f5a9cf684b70b959dda81d2d9dafa834d27fb68
Quint <me@di-wu.be>
2021-04-17 18:40:33 +0200

    Upgrade filter parser to v2.1.3.

---
+++ internal/filter/filter_v2.go
@@ -0,0 +1,337 @@
+package filter
+
+import (
+	"fmt"
+	"github.com/elimity-com/scim/schema"
+	"github.com/scim2/filter-parser/v2"
+	"log"
+)
+
+// validateAttributePath checks whether the given attribute path is a valid path within the given reference schema.
+func validateAttributePath(ref schema.Schema, attrPath filter.AttributePath) (schema.CoreAttribute, error) {
+	if uri := attrPath.URI(); uri != "" && uri != ref.ID {
+		return schema.CoreAttribute{}, fmt.Errorf("the uri does not match the schema id: %s", uri)
+	}
+
+	attr, ok := ref.Attributes.ContainsAttribute(attrPath.AttributeName)
+	if !ok {
+		return schema.CoreAttribute{}, fmt.Errorf(
+			"the reference schema does not have an attribute with the name: %s",
+			attrPath.AttributeName,
+		)
+	}
+	// e.g. name.givenName
+	//           ^________
+	if subAttrName := attrPath.SubAttributeName(); subAttrName != "" {
+		if err := validateSubAttribute(attr, subAttrName); err != nil {
+			return schema.CoreAttribute{}, err
+		}
+	}
+	return attr, nil
+}
+
+// validateExpression checks whether the given expression is a valid expression within the given reference schema.
+func validateExpression(ref schema.Schema, e filter.Expression) error {
+	switch e := e.(type) {
+	case *filter.ValuePath:
+		attr, err := validateAttributePath(ref, e.AttributePath)
+		if err != nil {
+			return nil
+		}
+		if err := validateExpression(
+			schema.Schema{
+				ID:         ref.ID,
+				Attributes: attr.SubAttributes(),
+			},
+			e.ValueFilter,
+		); err != nil {
+			return err
+		}
+	case *filter.AttributeExpression:
+		if _, err := validateAttributePath(ref, e.AttributePath); err != nil {
+			return err
+		}
+	case *filter.LogicalExpression:
+		if err := validateExpression(ref, e.Left); err != nil {
+			return err
+		}
+		if err := validateExpression(ref, e.Right); err != nil {
+			return err
+		}
+	case *filter.NotExpression:
+		if err := validateExpression(ref, e.Expression); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("unknown expression type: %v", e)
+	}
+	return nil
+}
+
+// validateSubAttribute checks whether the given attribute name is a attribute within the given reference attribute.
+func validateSubAttribute(attr schema.CoreAttribute, subAttrName string) error {
+	if !attr.HasSubAttributes() {
+		return fmt.Errorf("the attribute has no sub-attributes")
+	}
+
+	if _, ok := attr.SubAttributes().ContainsAttribute(subAttrName); !ok {
+		return fmt.Errorf("the attribute has no sub-attributes named: %s", subAttrName)
+	}
+	return nil
+}
+
+// PathValidator represents a path validator.
+type PathValidator struct {
+	path       filter.Path
+	schema     schema.Schema
+	extensions []schema.Schema
+}
+
+// NewPathValidator constructs a new path validator.
+func NewPathValidator(p filter.Path, s schema.Schema, exts ...schema.Schema) PathValidator {
+	return PathValidator{
+		path:       p,
+		schema:     s,
+		extensions: exts,
+	}
+}
+
+// Validate checks whether the path is a valid path within the given reference schemas.
+func (v PathValidator) Validate() error {
+	err := v.validatePath(v.schema)
+	if err == nil {
+		return nil
+	}
+	for _, e := range v.extensions {
+		if err := v.validatePath(e); err == nil {
+			return nil
+		}
+	}
+	return err
+}
+
+// validatePath tries to validate the path against the given schema.
+func (v PathValidator) validatePath(ref schema.Schema) error {
+	// e.g. members
+	//      ^______
+	attr, err := validateAttributePath(ref, v.path.AttributePath)
+	if err != nil {
+		return err
+	}
+
+	// e.g. members[value eq "0"]
+	//			   ^_____________
+	if v.path.ValueExpression != nil {
+		if err := validateExpression(
+			schema.Schema{
+				ID:         ref.ID,
+				Attributes: attr.SubAttributes(),
+			},
+			v.path.ValueExpression,
+		); err != nil {
+			return err
+		}
+	}
+
+	// e.g. members[value eq "0"].displayName
+	//			                  ^__________
+	if subAttrName := v.path.SubAttributeName(); subAttrName != "" {
+		if err := validateSubAttribute(attr, subAttrName); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Validator represents a filter validator.
+type Validator struct {
+	filter     filter.Expression
+	schema     schema.Schema
+	extensions []schema.Schema
+}
+
+// NewValidator constructs a new path validator.
+func NewValidator(e filter.Expression, s schema.Schema, exts ...schema.Schema) Validator {
+	return Validator{
+		filter:     e,
+		schema:     s,
+		extensions: exts,
+	}
+}
+
+// PassesFilter checks whether given resources passes the filter.
+func (v Validator) PassesFilter(resource map[string]interface{}) bool {
+	switch e := v.filter.(type) {
+	case *filter.ValuePath:
+		ref, attr, ok := v.referenceContains(e.AttributePath)
+		if !ok {
+			// Could not find an attribute that matches the attribute path.
+			return false
+		}
+		if !attr.MultiValued() {
+			// Value path filters can only be applied to multi-valued attributes.
+			return false
+		}
+
+		value, ok := resource[attr.Name()]
+		if !ok {
+			// Also try with the id as prefix.
+			value, ok = resource[fmt.Sprintf("%s:%s", ref.ID, attr.Name())]
+			if !ok {
+				// The give resource does not have the wanted attribute.
+				return false
+			}
+		}
+		valueFilter := NewValidator(
+			e.ValueFilter,
+			schema.Schema{
+				ID:         ref.ID,
+				Attributes: attr.SubAttributes(),
+			},
+		)
+		switch value := value.(type) {
+		case []interface{}:
+			for _, a := range value {
+				attr, ok := a.(map[string]interface{})
+				if !ok {
+					return false
+				}
+				if valueFilter.PassesFilter(attr) {
+					return true
+				}
+			}
+		}
+		return false
+	case *filter.AttributeExpression:
+		ref, attr, ok := v.referenceContains(e.AttributePath)
+		if !ok {
+			// Could not find an attribute that matches the attribute path.
+			return false
+		}
+
+		value, ok := resource[attr.Name()]
+		if !ok {
+			// Also try with the id as prefix.
+			value, ok = resource[fmt.Sprintf("%s:%s", ref.ID, attr.Name())]
+			if !ok {
+				// The give resource does not have the wanted attribute.
+				return false
+			}
+		}
+
+		var (
+			// cmpAttr will be the attribute to validate the filter against.
+			cmpAttr = attr
+
+			subAttr     schema.CoreAttribute
+			subAttrName = e.AttributePath.SubAttributeName()
+		)
+
+		if subAttrName != "" {
+			if !attr.HasSubAttributes() {
+				// The attribute has no sub-attributes.
+				return false
+			}
+			subAttr, ok = attr.SubAttributes().ContainsAttribute(subAttrName)
+			if !ok {
+				return false
+			}
+
+			attr, ok := value.(map[string]interface{})
+			if !ok {
+				return false
+			}
+			value, ok = attr[subAttr.Name()]
+			if !ok {
+				return false
+			}
+
+			cmpAttr = subAttr
+		}
+
+		cmp, err := createCompareFunction(e, cmpAttr)
+		if err != nil {
+			// TODO replace booleans w/ errors.
+			log.Println(err)
+			return false
+		}
+
+		if !attr.MultiValued() {
+			return cmp(value)
+		}
+
+		switch value := value.(type) {
+		case []interface{}:
+			for _, v := range value {
+				if cmp(v) {
+					return true
+				}
+			}
+		}
+	case *filter.LogicalExpression:
+		switch e.Operator {
+		case filter.AND:
+			if !NewValidator(
+				e.Left,
+				v.schema,
+				v.extensions...,
+			).PassesFilter(resource) {
+				return false
+			}
+			return NewValidator(
+				e.Right,
+				v.schema,
+				v.extensions...,
+			).PassesFilter(resource)
+		case filter.OR:
+			if NewValidator(
+				e.Left,
+				v.schema,
+				v.extensions...,
+			).PassesFilter(resource) {
+				return true
+			}
+			return NewValidator(
+				e.Right,
+				v.schema,
+				v.extensions...,
+			).PassesFilter(resource)
+		}
+	case *filter.NotExpression:
+		if !NewValidator(
+			e.Expression,
+			v.schema,
+			v.extensions...,
+		).PassesFilter(resource) {
+			return true
+		}
+	}
+	return false
+}
+
+// Validate checks whether the expression is a valid path within the given reference schemas.
+func (v Validator) Validate() error {
+	err := validateExpression(v.schema, v.filter)
+	if err == nil {
+		return nil
+	}
+	for _, e := range v.extensions {
+		if err := validateExpression(e, v.filter); err == nil {
+			return nil
+		}
+	}
+	return err
+}
+
+// referenceContains returns the schema and attribute to which the attribute path applies.
+func (v Validator) referenceContains(attrPath filter.AttributePath) (schema.Schema, schema.CoreAttribute, bool) {
+	for _, s := range append([]schema.Schema{v.schema}, v.extensions...) {
+		if uri := attrPath.URI(); uri != "" && s.ID != uri {
+			continue
+		}
+		if attr, ok := s.Attributes.ContainsAttribute(attrPath.AttributeName); ok {
+			return s, attr, true
+		}
+	}
+	return schema.Schema{}, schema.CoreAttribute{}, false
+}
---
+++ internal/filter/operators.go
@@ -0,0 +1,307 @@
+package filter
+
+import (
+	"fmt"
+	datetime "github.com/di-wu/xsd-datetime"
+	"github.com/elimity-com/scim/schema"
+	"github.com/scim2/filter-parser/v2"
+	"strings"
+	"time"
+)
+
+// createCompareFunction returns a compare function based on the attribute expression and attribute.
+// e.g. `userName eq "john"` will return a string comparator that checks whether the passed value is equal to "john".
+func createCompareFunction(e *filter.AttributeExpression, attr schema.CoreAttribute) (func(interface{}) bool, error) {
+	switch typ := attr.AttributeType(); typ {
+	case "binary":
+		ref, ok := e.CompareValue.(string)
+		if !ok {
+			return nil, fmt.Errorf("a binary attribute needs to be compared to a string")
+		}
+		switch e.Operator {
+		case filter.EQ:
+			return strComp(ref, attr, func(v, ref string) bool { return v == ref })
+		case filter.NE:
+			return strComp(ref, attr, func(v, ref string) bool { return v != ref })
+		case filter.CO:
+			return strComp(ref, attr, strings.Contains)
+		case filter.SW:
+			return strComp(ref, attr, strings.HasPrefix)
+		case filter.EW:
+			return strComp(ref, attr, strings.HasSuffix)
+		case filter.GT, filter.LT, filter.GE, filter.LE:
+			return nil, fmt.Errorf("invalid attribute type %q", typ)
+		default:
+			return func(value interface{}) bool {
+				return false
+			}, fmt.Errorf("unknown operator in expression: %s", e)
+		}
+	case "dateTime":
+		date, ok := e.CompareValue.(string)
+		if !ok {
+			return nil, fmt.Errorf("a dateTime attribute needs to be compared to a string")
+		}
+		ref, err := datetime.Parse(date)
+		if err != nil {
+			return nil, fmt.Errorf("a dateTime attribute needs to be compared to a dateTime")
+		}
+		switch e.Operator {
+		case filter.EQ:
+			return cmpDataTime(ref, func(v, ref time.Time) bool {
+				return v.Equal(ref)
+			}), nil
+		case filter.NE:
+			return cmpDataTime(ref, func(v, ref time.Time) bool {
+				return !v.Equal(ref)
+			}), nil
+		case filter.CO:
+			return strComp(date, attr, strings.Contains)
+		case filter.SW:
+			return strComp(date, attr, strings.HasPrefix)
+		case filter.EW:
+			return strComp(date, attr, strings.HasSuffix)
+		case filter.GT:
+			return cmpDataTime(ref, func(v, ref time.Time) bool {
+				return v.After(ref)
+			}), nil
+		case filter.LT:
+			return cmpDataTime(ref, func(v, ref time.Time) bool {
+				return v.Before(ref)
+			}), nil
+		case filter.GE:
+			return cmpDataTime(ref, func(v, ref time.Time) bool {
+				return v.After(ref) || v.Equal(ref)
+			}), nil
+		case filter.LE:
+			return cmpDataTime(ref, func(v, ref time.Time) bool {
+				return v.Before(ref) || v.Equal(ref)
+			}), nil
+		default:
+			return func(value interface{}) bool {
+				return false
+			}, fmt.Errorf("unknown operator in expression: %s", e)
+		}
+	case "reference", "string":
+		ref, ok := e.CompareValue.(string)
+		if !ok {
+			return nil, fmt.Errorf("a dateTime attribute needs to be compared to a string")
+		}
+		switch e.Operator {
+		case filter.EQ:
+			return strComp(ref, attr, func(v, ref string) bool { return v == ref })
+		case filter.NE:
+			return strComp(ref, attr, func(v, ref string) bool { return v != ref })
+		case filter.CO:
+			return strComp(ref, attr, strings.Contains)
+		case filter.SW:
+			return strComp(ref, attr, strings.HasPrefix)
+		case filter.EW:
+			return strComp(ref, attr, strings.HasSuffix)
+		case filter.GT:
+			return strComp(ref, attr, func(v, ref string) bool { return strings.Compare(v, ref) > 0 })
+		case filter.LT:
+			return strComp(ref, attr, func(v, ref string) bool { return strings.Compare(v, ref) < 0 })
+		case filter.GE:
+			return strComp(ref, attr, func(v, ref string) bool { return strings.Compare(v, ref) >= 0 })
+		case filter.LE:
+			return strComp(ref, attr, func(v, ref string) bool { return strings.Compare(v, ref) <= 0 })
+		default:
+			return func(value interface{}) bool {
+				return false
+			}, fmt.Errorf("unknown operator in expression: %s", e)
+		}
+	case "boolean":
+		ref, ok := e.CompareValue.(bool)
+		if !ok {
+			return nil, fmt.Errorf("a boolean attribute needs to be compared to a boolean")
+		}
+		switch e.Operator {
+		case filter.EQ:
+			return cmpBool(ref, func(v, ref bool) bool { return v == ref }), nil
+		case filter.NE:
+			return cmpBool(ref, func(v, ref bool) bool { return v != ref }), nil
+		case filter.CO:
+			return strComp(ref, attr, strings.Contains)
+		case filter.SW:
+			return strComp(ref, attr, strings.HasPrefix)
+		case filter.EW:
+			return strComp(ref, attr, strings.HasSuffix)
+		case filter.GT, filter.LT, filter.GE, filter.LE:
+			return nil, fmt.Errorf("invalid attribute type %q", typ)
+		default:
+			return func(value interface{}) bool {
+				return false
+			}, fmt.Errorf("unknown operator in expression: %s", e)
+		}
+	case "decimal":
+		ref, ok := toFloat(e.CompareValue)
+		if !ok {
+			return nil, fmt.Errorf("a dateTime attribute needs to be compared to a string")
+		}
+		switch e.Operator {
+		case filter.EQ:
+			return cmpDecimal(ref, func(v, ref float64) bool { return v == ref }), nil
+		case filter.NE:
+			return cmpDecimal(ref, func(v, ref float64) bool { return v != ref }), nil
+		case filter.CO:
+			return strComp(ref, attr, strings.Contains)
+		case filter.SW:
+			return strComp(ref, attr, strings.HasPrefix)
+		case filter.EW:
+			return strComp(ref, attr, strings.HasSuffix)
+		case filter.GT:
+			return cmpDecimal(ref, func(v, ref float64) bool { return v > ref }), nil
+		case filter.LT:
+			return cmpDecimal(ref, func(v, ref float64) bool { return v < ref }), nil
+		case filter.GE:
+			return cmpDecimal(ref, func(v, ref float64) bool { return v >= ref }), nil
+		case filter.LE:
+			return cmpDecimal(ref, func(v, ref float64) bool { return v <= ref }), nil
+		default:
+			return func(value interface{}) bool {
+				return false
+			}, fmt.Errorf("unknown operator in expression: %s", e)
+		}
+	case "integer":
+		ref, ok := toInt(e.CompareValue)
+		if !ok {
+			return nil, fmt.Errorf("a dateTime attribute needs to be compared to a string")
+		}
+		switch e.Operator {
+		case filter.EQ:
+			return cmpInteger(ref, func(v, ref int) bool { return v == ref }), nil
+		case filter.NE:
+			return cmpInteger(ref, func(v, ref int) bool { return v != ref }), nil
+		case filter.CO:
+			return strComp(ref, attr, strings.Contains)
+		case filter.SW:
+			return strComp(ref, attr, strings.HasPrefix)
+		case filter.EW:
+			return strComp(ref, attr, strings.HasSuffix)
+		case filter.GT:
+			return cmpInteger(ref, func(v, ref int) bool { return v > ref }), nil
+		case filter.LT:
+			return cmpInteger(ref, func(v, ref int) bool { return v < ref }), nil
+		case filter.GE:
+			return cmpInteger(ref, func(v, ref int) bool { return v >= ref }), nil
+		case filter.LE:
+			return cmpInteger(ref, func(v, ref int) bool { return v <= ref }), nil
+		default:
+			return func(value interface{}) bool {
+				return false
+			}, fmt.Errorf("unknown operator in expression: %s", e)
+		}
+	default:
+		panic(fmt.Sprintf("unknown attribute type: %s", typ))
+	}
+}
+
+func cmpDataTime(ref time.Time, cmp func(v, ref time.Time) bool) func(interface{}) bool {
+	return func(i interface{}) bool {
+		date, ok := i.(string)
+		if !ok {
+			return false
+		}
+		value, err := datetime.Parse(date)
+		if err != nil {
+			return false
+		}
+		return cmp(value, ref)
+	}
+}
+
+func cmpInteger(ref int, cmp func(v, ref int) bool) func(interface{}) bool {
+	return func(i interface{}) bool {
+		value, ok := toInt(i)
+		if !ok {
+			return false
+		}
+		return cmp(value, ref)
+	}
+}
+
+func cmpDecimal(ref float64, cmp func(v, ref float64) bool) func(interface{}) bool {
+	return func(i interface{}) bool {
+		value, ok := toFloat(i)
+		if !ok {
+			return false
+		}
+		return cmp(value, ref)
+	}
+}
+
+func cmpBool(ref bool, cmp func(v, ref bool) bool) func(interface{}) bool {
+	return func(i interface{}) bool {
+		value, ok := i.(bool)
+		if !ok {
+			return false
+		}
+		return cmp(value, ref)
+	}
+}
+
+// The entire operator value must be a substring of the attribute value for a match.
+func strComp(ref interface{}, attr schema.CoreAttribute, cmp func(v, ref string) bool) (func(interface{}) bool, error) {
+	switch typ := attr.AttributeType(); typ {
+	case "boolean":
+		ref, ok := ref.(bool)
+		if !ok {
+			return nil, fmt.Errorf("a boolean attribute needs to be compared to a boolean")
+		}
+		return func(i interface{}) bool {
+			value, ok := i.(bool)
+			if !ok {
+				return false
+			}
+			return cmp(fmt.Sprintf("%t", value), fmt.Sprintf("%t", ref))
+		}, nil
+	case "decimal":
+		ref, ok := toFloat(ref)
+		if !ok {
+			return nil, fmt.Errorf("a decimal attribute needs to be compared to a float/int")
+		}
+		return func(i interface{}) bool {
+			value, ok := toFloat(i)
+			if !ok {
+				return false
+			}
+			// fmt.Sprintf("%f") would give them both the same precision.
+			return cmp(fmt.Sprint(value), fmt.Sprint(ref))
+		}, nil
+	case "integer":
+		ref, ok := toInt(ref)
+		if !ok {
+			return nil, fmt.Errorf("a integer attribute needs to be compared to a int")
+		}
+		return func(i interface{}) bool {
+			value, ok := toInt(i)
+			if !ok {
+				return false
+			}
+			return cmp(fmt.Sprintf("%d", value), fmt.Sprintf("%d", ref))
+		}, nil
+	case "binary", "reference", "dateTime", "string":
+		ref, ok := ref.(string)
+		if !ok {
+			return nil, fmt.Errorf("a %s attribute needs to be compared to a string", typ)
+		}
+		if attr.CaseExact() {
+			return func(i interface{}) bool {
+				value, ok := i.(string)
+				if !ok {
+					return false
+				}
+				return cmp(value, ref)
+			}, nil
+		}
+		return func(i interface{}) bool {
+			value, ok := i.(string)
+			if !ok {
+				return false
+			}
+			return cmp(strings.ToLower(value), strings.ToLower(ref))
+		}, nil
+	default:
+		panic(fmt.Sprintf("unknown attribute type: %s", typ))
+	}
+}
