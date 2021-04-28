06f84390db380d4144d93df367aa3455883744bc
Quint <me@di-wu.be>
2021-04-28 11:24:01 +0200

    Add support for sub-attribute assignments in complex attributes.

--- internal/patch/add.go
+++ internal/patch/add.go
@@ -23,46 +23,9 @@ func (v OperationValidator) ValidateAdd() error {
 		return fmt.Errorf("an add operation does not support value expressions")
 	}
 
-	var (
-		refSchema = v.schema
-		attrPath  = v.path.AttributePath
-		attrName  = attrPath.AttributeName
-	)
-	if uri := attrPath.URI(); uri != "" {
-		// It can only be an extension if it has a uri prefix.
-		var ok bool
-		if refSchema, ok = v.schemas[uri]; !ok {
-			return fmt.Errorf("invalid uri prefix: %s", uri)
-		}
-	}
-
-	var refAttr *schema.CoreAttribute
-	for _, attr := range refSchema.Attributes {
-		if strings.EqualFold(attr.Name(), attrName) {
-			refAttr = &attr
-			break
-		}
-	}
-	if refAttr == nil {
-		// This should not occur since the filter parser will validate this.
-		return fmt.Errorf("could not find attribute %s", v.path)
-	}
-	if subAttrName := attrPath.SubAttributeName(); subAttrName != "" {
-		if !refAttr.HasSubAttributes() {
-			return fmt.Errorf("the referred attribute has no sub-attributes: %s", v.path)
-		}
-
-		var refSubAttr *schema.CoreAttribute
-		for _, attr := range refAttr.SubAttributes() {
-			if strings.EqualFold(attr.Name(), subAttrName) {
-				refSubAttr = &attr
-				break
-			}
-		}
-		if refSubAttr == nil {
-			return fmt.Errorf("could not find attribute %s", v.path)
-		}
-		refAttr = refSubAttr
+	refAttr, err := v.getRefAttribute(v.path.AttributePath)
+	if err != nil {
+		return err
 	}
 
 	if refAttr.MultiValued() {
@@ -101,27 +64,13 @@ func (v OperationValidator) validateAddEmptyPath() error {
 
 	for p, value := range attributes {
 		path, err := filter.ParseAttrPath([]byte(p))
-		if err != nil || path.SubAttribute != nil {
+		if err != nil {
 			return fmt.Errorf("invalid attribute path: %s", p)
 		}
-		refSchema := v.schema
-		if uri := path.URI(); uri != "" {
-			// It can only be an extension if it has a uri prefix.
-			var ok bool
-			if refSchema, ok = v.schemas[uri]; !ok {
-				return fmt.Errorf("invalid uri prefix: %s", uri)
-			}
-		}
 
-		var refAttr *schema.CoreAttribute
-		for _, attr := range refSchema.Attributes {
-			if strings.EqualFold(attr.Name(), path.AttributeName) {
-				refAttr = &attr
-				break
-			}
-		}
-		if refAttr == nil {
-			return fmt.Errorf("could not find attribute %s", path)
+		refAttr, err := v.getRefAttribute(path)
+		if err != nil {
+			return err
 		}
 
 		if refAttr.MultiValued() {
@@ -151,4 +100,56 @@ func (v OperationValidator) validateAddEmptyPath() error {
 		}
 	}
 	return nil
+}
+
+// getRefAttribute returns the corresponding attribute based on the given attribute path.
+//
+// e.g.
+//  - `userName` would return the userNameAttribute.
+//	- `name.givenName` would return the `givenName` attribute.
+//  - `ext:employeeNumber` would return the `employeeNumber` from the extension.
+func (v OperationValidator) getRefAttribute(attrPath filter.AttributePath) (*schema.CoreAttribute, error) {
+	// Get the corresponding schema, this can be the main schema or an extension.
+	var refSchema = v.schema
+	if uri := attrPath.URI(); uri != "" {
+		// It can only be an extension if it has a uri prefix.
+		var ok bool
+		if refSchema, ok = v.schemas[uri]; !ok {
+			return nil, fmt.Errorf("invalid uri prefix: %s", uri)
+		}
+	}
+
+	// Get the correct attribute corresponding to the given attribute path.
+	var (
+		refAttr  *schema.CoreAttribute
+		attrName = attrPath.AttributeName
+	)
+	for _, attr := range refSchema.Attributes {
+		if strings.EqualFold(attr.Name(), attrName) {
+			refAttr = &attr
+			break
+		}
+	}
+	if refAttr == nil {
+		// This should not occur since the filter parser will validate this.
+		return nil, fmt.Errorf("could not find attribute %s", v.path)
+	}
+	if subAttrName := attrPath.SubAttributeName(); subAttrName != "" {
+		if !refAttr.HasSubAttributes() {
+			return nil, fmt.Errorf("the referred attribute has no sub-attributes: %s", v.path)
+		}
+
+		var refSubAttr *schema.CoreAttribute
+		for _, attr := range refAttr.SubAttributes() {
+			if strings.EqualFold(attr.Name(), subAttrName) {
+				refSubAttr = &attr
+				break
+			}
+		}
+		if refSubAttr == nil {
+			return nil, fmt.Errorf("could not find attribute %s", v.path)
+		}
+		refAttr = refSubAttr
+	}
+	return refAttr, nil
 }
