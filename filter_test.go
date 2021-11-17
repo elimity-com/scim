136e0ab140f999b1172e7475a6831321ef7bde3d
Davy Van Den Steen <17579331+xonstone@users.noreply.github.com>
2021-11-17 17:10:19 +0100

    Goarrange

--- filter_test.go
+++ filter_test.go
@@ -13,38 +13,57 @@ import (
 	"github.com/elimity-com/scim/schema"
 )
 
-func newTestServerForFilter() scim.Server {
-	return scim.Server{
-		ResourceTypes: []scim.ResourceType{
-			{
-				ID:          optional.NewString("User"),
-				Name:        "User",
-				Endpoint:    "/Users",
-				Description: optional.NewString("User Account"),
-				Schema:      schema.CoreUserSchema(),
-				Handler: &testResourceHandler{
-					data: map[string]testData{
-						"0001": {attributes: map[string]interface{}{"userName": "testUser"}},
-						"0002": {attributes: map[string]interface{}{"userName": "testUser+test"}},
-					},
-					schema: schema.CoreUserSchema(),
-				},
-			},
-			{
-				ID:          optional.NewString("Group"),
-				Name:        "Group",
-				Endpoint:    "/Groups",
-				Description: optional.NewString("Group"),
-				Schema:      schema.CoreGroupSchema(),
-				Handler: &testResourceHandler{
-					data: map[string]testData{
-						"0001": {attributes: map[string]interface{}{"displayName": "testGroup"}},
-						"0002": {attributes: map[string]interface{}{"displayName": "testGroup+test"}},
-					},
-					schema: schema.CoreGroupSchema(),
-				},
-			},
-		},
+func Test_Group_Filter(t *testing.T) {
+	s := newTestServerForFilter()
+
+	tests := []struct {
+		name                string
+		filter              string
+		expectedDisplayName string
+	}{
+		{name: "Happy path", filter: "displayName eq \"testGroup\"", expectedDisplayName: "testGroup"},
+		{name: "Happy path with plus sign", filter: "displayName eq \"testGroup+test\"", expectedDisplayName: "testGroup+test"},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			r := httptest.NewRequest(http.MethodGet, "/Groups?filter="+url.QueryEscape(tt.filter), nil)
+			w := httptest.NewRecorder()
+			s.ServeHTTP(w, r)
+
+			bytes, err := ioutil.ReadAll(w.Result().Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if w.Result().StatusCode != http.StatusOK {
+				t.Fatal(w.Result().StatusCode, string(bytes))
+			}
+
+			var result map[string]interface{}
+			if err := json.Unmarshal(bytes, &result); err != nil {
+				t.Fatal(err)
+			}
+
+			resources, ok := result["Resources"].([]interface{})
+			if !ok {
+				t.Fatal("Resources is not the right type or missing")
+			}
+
+			firstResource, ok := resources[0].(map[string]interface{})
+			if !ok {
+				t.Fatal("first Resource is not the right type or missing")
+			}
+
+			displayName, ok := firstResource["displayName"].(string)
+			if !ok {
+				t.Fatal("displayName is not the right type or missing")
+			}
+
+			if displayName != tt.expectedDisplayName {
+				t.Fatal("displayName not eq " + displayName)
+			}
+		})
 	}
 }
 
@@ -102,56 +121,37 @@ func Test_User_Filter(t *testing.T) {
 	}
 }
 
-func Test_Group_Filter(t *testing.T) {
-	s := newTestServerForFilter()
-
-	tests := []struct {
-		name                string
-		filter              string
-		expectedDisplayName string
-	}{
-		{name: "Happy path", filter: "displayName eq \"testGroup\"", expectedDisplayName: "testGroup"},
-		{name: "Happy path with plus sign", filter: "displayName eq \"testGroup+test\"", expectedDisplayName: "testGroup+test"},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			r := httptest.NewRequest(http.MethodGet, "/Groups?filter="+url.QueryEscape(tt.filter), nil)
-			w := httptest.NewRecorder()
-			s.ServeHTTP(w, r)
-
-			bytes, err := ioutil.ReadAll(w.Result().Body)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if w.Result().StatusCode != http.StatusOK {
-				t.Fatal(w.Result().StatusCode, string(bytes))
-			}
-
-			var result map[string]interface{}
-			if err := json.Unmarshal(bytes, &result); err != nil {
-				t.Fatal(err)
-			}
-
-			resources, ok := result["Resources"].([]interface{})
-			if !ok {
-				t.Fatal("Resources is not the right type or missing")
-			}
-
-			firstResource, ok := resources[0].(map[string]interface{})
-			if !ok {
-				t.Fatal("first Resource is not the right type or missing")
-			}
-
-			displayName, ok := firstResource["displayName"].(string)
-			if !ok {
-				t.Fatal("displayName is not the right type or missing")
-			}
-
-			if displayName != tt.expectedDisplayName {
-				t.Fatal("displayName not eq " + displayName)
-			}
-		})
+func newTestServerForFilter() scim.Server {
+	return scim.Server{
+		ResourceTypes: []scim.ResourceType{
+			{
+				ID:          optional.NewString("User"),
+				Name:        "User",
+				Endpoint:    "/Users",
+				Description: optional.NewString("User Account"),
+				Schema:      schema.CoreUserSchema(),
+				Handler: &testResourceHandler{
+					data: map[string]testData{
+						"0001": {attributes: map[string]interface{}{"userName": "testUser"}},
+						"0002": {attributes: map[string]interface{}{"userName": "testUser+test"}},
+					},
+					schema: schema.CoreUserSchema(),
+				},
+			},
+			{
+				ID:          optional.NewString("Group"),
+				Name:        "Group",
+				Endpoint:    "/Groups",
+				Description: optional.NewString("Group"),
+				Schema:      schema.CoreGroupSchema(),
+				Handler: &testResourceHandler{
+					data: map[string]testData{
+						"0001": {attributes: map[string]interface{}{"displayName": "testGroup"}},
+						"0002": {attributes: map[string]interface{}{"displayName": "testGroup+test"}},
+					},
+					schema: schema.CoreGroupSchema(),
+				},
+			},
+		},
 	}
 }
