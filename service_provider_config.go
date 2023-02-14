dda3c9f3c3d8840c0a66f5fe98a1d3478e0be414
Siqing Zheng <szheng@singlestore.com>
2023-02-14 15:59:52 -0800

    add prefix in config and trim it in ServeHttp

--- service_provider_config.go
+++ service_provider_config.go
@@ -40,6 +40,8 @@ const (
 // ServiceProviderConfig enables a service provider to discover SCIM specification features in a standardized form as
 // well as provide additional implementation details to clients.
 type ServiceProviderConfig struct {
+	// PathPrefix is the prefix in the SCIM API URL used to trim the request path in ServeHTTP
+	PathPrefix string
 	// DocumentationURI is an HTTP-addressable URL pointing to the service provider's human-consumable help
 	// documentation.
 	DocumentationURI optional.String
