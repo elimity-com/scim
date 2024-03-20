b147e7679de9e4ae0a841f628ae4fe188c41bbb5
Dmitrii Prisacari <prisacari.dmitrii@gmail.com>
2024-03-20 10:21:27 +0000

    fix: validate schema ID in the provided resource

--- schema/schema.go
+++ schema/schema.go
@@ -127,10 +127,39 @@ func (s Schema) getRawAttributes() []map[string]interface{} {
 	return attributes
 }
 
+func (s Schema) validateSchemaID(resource map[string]interface{}) *errors.ScimError {
+	resourceSchemas, present := resource["schemas"]
+	if !present {
+		return &errors.ScimErrorInvalidSyntax
+	}
+
+	resourceSchemasSlice, ok := resourceSchemas.([]interface{})
+	if !ok {
+		return &errors.ScimErrorInvalidSyntax
+	}
+
+	var schemaFound bool
+	for _, v := range resourceSchemasSlice {
+		if v == s.ID {
+			schemaFound = true
+			break
+		}
+	}
+	if !schemaFound {
+		return &errors.ScimErrorInvalidSyntax
+	}
+
+	return nil
+}
+
 func (s Schema) validate(resource interface{}, checkMutability bool) (map[string]interface{}, *errors.ScimError) {
 	core, ok := resource.(map[string]interface{})
 	if !ok {
 		return nil, &errors.ScimErrorInvalidSyntax
+	}
+
+	if err := s.validateSchemaID(core); err != nil {
+		return nil, err
 	}
 
 	attributes := make(map[string]interface{})
