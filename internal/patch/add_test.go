96da93b774a42e708bc25eb0106c84931ebbc692
Quint Daenen <quint@daenen.email>
2026-07-22 09:01:45 +0200

    chore: satisfy goarrange in add_test

--- internal/patch/add_test.go
+++ internal/patch/add_test.go
@@ -6,6 +6,25 @@ import (
 	"github.com/elimity-com/scim/schema"
 )
 
+// The following example shows how to add extension attributes to a User resource without using a "path" attribute,
+// where the extension is provided as a nested object keyed by its schema URI. See RFC 7643 Section 3.3
+// and RFC 7644 Section 3.5.2.
+func Example_addExtensionWithoutPath() {
+	operation, _ := json.Marshal(map[string]interface{}{
+		"op": "add",
+		"value": map[string]interface{}{
+			"userName": "test",
+			"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": map[string]interface{}{
+				"organization": "SUSE",
+			},
+		},
+	})
+	validator, _ := NewValidator(operation, schema.CoreUserSchema(), schema.ExtensionEnterpriseUser())
+	fmt.Println(validator.Validate())
+	// Output:
+	// map[urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:map[organization:SUSE] userName:test] <nil>
+}
+
 // The following example shows how to add a member to a group.
 func Example_addMemberToGroup() {
 	operation, _ := json.Marshal(map[string]interface{}{
@@ -41,23 +60,4 @@ func Example_addWithoutPath() {
 	fmt.Println(validator.Validate())
 	// Output:
 	// map[emails:[map[type:work value:quint@elimity.com]] nickname:di-wu] <nil>
-}
-
-// The following example shows how to add extension attributes to a User resource without using a "path" attribute,
-// where the extension is provided as a nested object keyed by its schema URI. See RFC 7643 Section 3.3
-// and RFC 7644 Section 3.5.2.
-func Example_addExtensionWithoutPath() {
-	operation, _ := json.Marshal(map[string]interface{}{
-		"op": "add",
-		"value": map[string]interface{}{
-			"userName": "test",
-			"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": map[string]interface{}{
-				"organization": "SUSE",
-			},
-		},
-	})
-	validator, _ := NewValidator(operation, schema.CoreUserSchema(), schema.ExtensionEnterpriseUser())
-	fmt.Println(validator.Validate())
-	// Output:
-	// map[urn:ietf:params:scim:schemas:extension:enterprise:2.0:User:map[organization:SUSE] userName:test] <nil>
 }
